c9d82460c32e45cbd74d1b746cec3ea2fb29d0bb
Vincent Herlemont <vincent@herlemont.fr>
2023-10-29 09:39:40 +0100

    fix: unknown result on macro

--- native_model_macro/src/method/decode_body.rs
+++ native_model_macro/src/method/decode_body.rs
@@ -6,7 +6,7 @@ pub(crate) fn generate_native_model_decode_body(attrs: &ModelAttributes) -> Toke
     let id = attrs.id.clone().expect("`id` is required");
     let with = attrs.with.clone().expect("`with` is required");
     let gen = quote! {
-        fn native_model_decode_body(data: Vec<u8>, id: u32) -> Result<Self, native_model::DecodeBodyError> {
+        fn native_model_decode_body(data: Vec<u8>, id: u32) -> std::result::Result<Self, native_model::DecodeBodyError> {
             println!("id: {}, {}", id, #id);
             if id != #id {
                 return Err(native_model::DecodeBodyError::MismatchedModelId);
--- native_model_macro/src/method/encode_body.rs
+++ native_model_macro/src/method/encode_body.rs
@@ -5,7 +5,7 @@ use quote::quote;
 pub(crate) fn generate_native_model_encode_body(attrs: &ModelAttributes) -> TokenStream {
     let with = attrs.with.clone().expect("`with` is required");
     let gen = quote! {
-        fn native_model_encode_body(&self) -> Result<Vec<u8>, native_model::EncodeBodyError> {
+        fn native_model_encode_body(&self) -> std::result::Result<Vec<u8>, native_model::EncodeBodyError> {
             use native_model::Encode;
             #with::encode(self).map_err(|e| native_model::EncodeBodyError {
                 msg: format!("{}", e),
